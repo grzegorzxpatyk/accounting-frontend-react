bc76ae953d14b652f12f03a453f921a6cbff9ff9
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-04-25 10:09:24 +0200

    implement newly added components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 import './App.scss';
 
 function App() {
     return (
         <div className="App">
-            <h1>Hello</h1>
-            <br />
-            <nav>
-                <Link to="/">Strona główna</Link>
-                <Link to="/kontakt">Kontakt</Link>
-            </nav>
-            <Outlet />
+            <Header />
+            <main>
+                <Outlet />
+            </main>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Home from './routes/Home';
 import Contact from './routes/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +11,7 @@ root.render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />}>
-                <Route index element={<p>Welcome on the landing page!</p>} />
+                <Route index element={<Home />} />
                 <Route path="kontakt" element={<Contact />} />
             </Route>
         </Routes>
