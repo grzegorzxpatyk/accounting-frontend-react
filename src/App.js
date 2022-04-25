d0a8c1c1c020c8ccf4d5d389bb43e329b61096d9
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-04-25 11:19:09 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.scss';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
             <main>
                 <Outlet />
             </main>
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer() {
+    return (
+        <footer>
+            <span>Madar &copy; 2022</span>
+        </footer>
+    );
+}
+
+export default Footer;
