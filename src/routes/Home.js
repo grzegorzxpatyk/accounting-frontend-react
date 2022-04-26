6b71d0934bc88b7d5bd0c5567320678321ed2c89
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-04-27 00:31:19 +0200

    add shaking hands img to landing page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Home.scss';
 import { Fade } from 'react-reveal';
+import shakingHands from '../assets/shakingHands.jpg';
 
 function Home() {
     return (
         <section className="landingPage">
             <Fade left>
                 <h1>Biuro rachunkowe MADAR</h1>
+            </Fade>
+            <Fade top>
+                <img src={shakingHands} alt="" />
             </Fade>
         </section>
     );
