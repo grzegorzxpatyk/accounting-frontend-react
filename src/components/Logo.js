9f851118938cb11cdba08f061b1f8a95644cea09
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-04-26 22:36:48 +0200

    add router link to logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Fade } from 'react-reveal';
+import { Link } from 'react-router-dom';
 import './Logo.scss';
 
 function Logo({ title }) {
     return (
-        <h1 className="logoBrand">
-            <Fade top cascade>
-                {title}
-            </Fade>
-        </h1>
+        <Link to="/">
+            <h1 className="logoBrand">
+                <Fade top cascade>
+                    {title}
+                </Fade>
+            </h1>
+        </Link>
     );
 }
 
