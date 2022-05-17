0696adfb7a516d674ae73534285ebac88f94ad41
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-05-17 23:12:41 +0200

    add the aside to the home route with text and call to action link & style it

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.scss';
 import { Fade } from 'react-reveal';
 import calculator from '../assets/calculator.jpg';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
@@ -10,8 +11,19 @@ function Home() {
                 <h1>Biuro rachunkowe MADAR</h1>
             </Fade>
             <Fade top>
-                <img src={calculator} alt="" />
                 <div className="overlay"></div>
+                <img src={calculator} alt="" />
+            </Fade>
+            <Fade right>
+                <aside>
+                    <p>
+                        Potrzebujesz usług związanych z księgowością lub
+                        prowadzeniem działalności gospodarczej?
+                    </p>
+                    <p>
+                        <Link to="/uslugi">Dowiedz się więcej 🠒</Link>
+                    </p>
+                </aside>
             </Fade>
         </section>
     );
