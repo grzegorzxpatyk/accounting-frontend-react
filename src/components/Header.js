a5627c8af420f75f8455e996f80d9b928abbbb81
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-05-20 22:08:36 +0200

    make the menu toggler statefull and add event handler for click event on navigation links

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,46 @@ import { Link } from 'react-router-dom';
 import Logo from './Logo';
 import { Fade } from 'react-reveal';
 import './Header.scss';
+import { useState } from 'react';
 
 function Header() {
+    const [isMenuVisible, setMenuVisible] = useState(false);
+
+    function handleNavLinkClick() {
+        if (isMenuVisible) {
+            setMenuVisible(false);
+        } else {
+            setMenuVisible(true);
+        }
+    }
+
     return (
         <header>
             <Logo title="MADAR" />
             <nav>
-                <input type="checkbox" id="toggler" />
+                <input
+                    type="checkbox"
+                    id="toggler"
+                    checked={isMenuVisible}
+                    onClick={handleNavLinkClick}
+                />
                 <label htmlFor="toggler">MENU</label>
                 <ul>
                     <Fade>
                         <li>
-                            <Link to="/">Strona główna</Link>
+                            <Link to="/" onClick={handleNavLinkClick}>
+                                Strona główna
+                            </Link>
                         </li>
                         <li>
-                            <Link to="/uslugi">Usługi</Link>
+                            <Link to="/uslugi" onClick={handleNavLinkClick}>
+                                Usługi
+                            </Link>
                         </li>
                         <li>
-                            <Link to="/kontakt">Kontakt</Link>
+                            <Link to="/kontakt" onClick={handleNavLinkClick}>
+                                Kontakt
+                            </Link>
                         </li>
                     </Fade>
                 </ul>
