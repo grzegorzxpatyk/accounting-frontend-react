97279acdc3e9701125472882915ce77c8e36547c
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-06-02 22:52:01 +0200

    remove animation from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Fade } from 'react-reveal';
 import './Footer.scss';
 
 function Footer() {
     return (
         <footer>
-            <div>
-                <Fade>Madar &copy; 2022</Fade>
-            </div>
+            <div>Madar &copy; 2022</div>
         </footer>
     );
 }
